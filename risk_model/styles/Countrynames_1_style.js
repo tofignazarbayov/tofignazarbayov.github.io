c6df5a69f188f37aaea1a4f8051b35d7de7d9c12
Tofig Nazarbayov <tofignazarbayov@gmail.com>
2022-09-23 00:02:12 +0400

    Added risk model created for Zaman Broker

---
+++ risk_model/styles/Countrynames_1_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_Countrynames_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    ''
+    var labelText = "";
+    size = 0;
+    var labelFont = "15.600000000000001px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("NAME") !== null) {
+        labelText = String(feature.get("NAME"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+
+    return style;
+};
